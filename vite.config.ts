dcb3f47fbce52e92f3331ce313211241944527e3
ReadyBlasrt <acarnaval96@gmail.com>
2025-06-19 15:36:49 +0400

    fix(web): fix configs

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,17 @@ export default defineConfig({
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
+      "~components": path.resolve(__dirname, "./src/components"),
+      "~features": path.resolve(__dirname, "./src/features"),
+      "~hooks": path.resolve(__dirname, "./src/hooks"),
+      "~pages": path.resolve(__dirname, "./src/pages"),
+      "~routes": path.resolve(__dirname, "./src/routes"),
+      "~services": path.resolve(__dirname, "./src/services"),
+      "~styles": path.resolve(__dirname, "./src/styles"),
+      "~utils": path.resolve(__dirname, "./src/utils"),
+      "~types": path.resolve(__dirname, "./src/types"),
+      "~themes": path.resolve(__dirname, "./src/themes"),
+      "~shared": path.resolve(__dirname, "./src/shared"),
     },
   },
 });
